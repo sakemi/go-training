d8355b83c251b0839705dd1af851ed8a663e8751
sakemi <satoshi.sakemi@gmail.com>
2017-05-20 20:54:39 +0900

    Added ch05.ex04.

---
+++ ch05/ex04/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode {
+		if n.Data == "a" || n.Data == "link" {
+			for _, e := range n.Attr {
+				if e.Key == "href" {
+					links = append(links, e.Val)
+				}
+			}
+		} else if n.Data == "img" || n.Data == "script" {
+			for _, e := range n.Attr {
+				if e.Key == "src" {
+					links = append(links, e.Val)
+				}
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
