b846923266cf7e61a2875aaa248ce2ac63843d20
sakemi <satoshi.sakemi@gmail.com>
2017-06-24 15:35:20 +0900

    Added ch08.ex12 and ch08.ex13.

---
+++ ch08/ex13/chat.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+const timeout = 5 //min
+
+type client struct {
+	ch   chan<- string
+	name string
+}
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string)
+)
+
+func broadcaster() {
+	clients := make(map[client]bool)
+	for {
+		select {
+		case msg := <-messages:
+			for cli := range clients {
+				cli.ch <- msg
+			}
+
+		case cli := <-entering:
+			clients[cli] = true
+			for c := range clients {
+				cli.ch <- c.name + " is in this room."
+			}
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.ch)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string)
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	cli := client{ch, who}
+	entering <- cli
+
+	go disconnecter(conn, ch)
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+
+	leaving <- cli
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
+	}
+}
+
+func disconnecter(conn net.Conn, ch <-chan string) {
+	cnt := 0
+	tick := time.Tick(1 * time.Minute)
+	for cnt < timeout {
+		select {
+		case <-ch:
+			cnt = 0
+		case <-tick:
+			cnt++
+		}
+	}
+	conn.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
